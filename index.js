4d554cf3e723dde16958b89aa5437ed8182f0237
name <eshwarteja9966@gmail.com>
2025-11-24 16:10:54 +0530

    saved merge index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function closure(){
         console.log("Balance :"+a+b+0.5);
     }
 }
-function c(){
+function b(){
     var z=0;
     var b=1;
     var gst=0.5
     return function deposit(){
-        console.log("Balance :"+a+b+0.5);
+        console.log("Balance :"+a+0.5);
     }
 }
 var Balance=closure();
