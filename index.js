b2b35729dbd38472896d3aa49b5bcfb4dd0fed3c
murali ithanaboina <murali.ithanaboina1998@gmail.com>
2025-11-24 15:23:00 +0530

    first commit closures

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //closures
 function closure(){
     var a=0;
+    var b=1;
     return function deposit(){
-        console.log("Balance :"+a);
+        console.log("Balance :"+a+b);
     }
 }
 var Balance=closure();
- Balance.deposit;
+ Balance();
