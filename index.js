ed56806d59077b63e33a1e4ecff50063b1b378d4
murali ithanaboina <murali.ithanaboina1998@gmail.com>
2025-11-24 15:42:57 +0530

    added gst

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 function closure(){
     var a=0;
     var b=1;
+    var gst=0.5
     return function deposit(){
-        console.log("Balance :"+a+b);
+        console.log("Balance :"+a+b+0.5);
     }
 }
 var Balance=closure();
