8b5a323ddffc5d970115f53328e36530ba43bbbe
murali ithanaboina <murali.ithanaboina1998@gmail.com>
2025-11-24 17:01:11 +0530

    added new function cl

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function closure(){
     var z=0;
     var b=1;
     var gst=0.5
-    var change="everything"
+    var change="everyting"
     return function deposit(){
         console.log("Balance :"+a+b+0.5);
     }
 }
 function c(){
-    var z=0;
+    var op=0;
     var b=1;
     var gst=0.5
     return function deposit(){
